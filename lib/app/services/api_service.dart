e0f65748be205ea6465f1db468471edc65877b40
Eventyret <simen.dehlin@gmail.com>
2020-05-11 17:53:00 +0100

    Added getEndpointData

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'api.dart';
@@ -21,7 +22,40 @@ class APIService {
       }
     }
     print(
-        'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+        'Request ${api.tokenUri} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData({
+    @required String accessToken,
+    @required Endpoint endpoint,
+  }) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http
+        .get(uri.toString(), headers: {'Authorization': 'Bearer $accessToken'});
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+      print(
+        'Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'data',
+    Endpoint.totalTests: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+    Endpoint.todayCases: 'data',
+    Endpoint.todayDeaths: 'data',
+    Endpoint.critical: 'data',
+  };
 }
