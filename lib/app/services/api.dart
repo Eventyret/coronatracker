39dd735565737999ca65863082b53d98e002d88b
Eventyret <simen.dehlin@gmail.com>
2020-05-11 17:29:42 +0100

    Added basePath

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,8 @@
 import 'package:coronavirus_tracker/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
 
+enum Endpoint { cases, totalTests, deaths, recovered }
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -9,6 +11,7 @@ class API {
 
   static final String host = 'apigw.nubentos.com';
   static final int port = 443;
+  static final String basePath = 't/nubentos.com/ncovapi/2.0.0';
 
   Uri tokenUri() => Uri(
         scheme: 'https',
@@ -17,4 +20,17 @@ class API {
         path: 'token',
         queryParameters: {'grant_type': 'client_credentials'},
       );
+
+  Uri endpointUri(Endpoint endpoint) => Uri(
+      scheme: 'https',
+      host: host,
+      port: port,
+      path: '$basePath/${_paths[endpoint]}');
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.totalTests: 'totalTests',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered'
+  };
 }
