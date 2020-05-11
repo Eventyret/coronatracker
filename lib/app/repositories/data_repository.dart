a60fcd1994c09ddcaaa5ad0d0a1016c6b0bfb54f
Eventyret <simen.dehlin@gmail.com>
2020-05-11 19:53:21 +0100

    Created endpoint repository

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,6 +1,7 @@
 import 'package:coronavirus_tracker/app/services/api.dart';
 import 'package:coronavirus_tracker/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   DataRepository({@required this.apiService});
@@ -8,10 +9,19 @@ class DataRepository {
   String _accessToken;
 
   Future<int> getEndpointData(Endpoint endpoint) async {
-    if (_accessToken == null) {
-      _accessToken = await apiService.getAccessToken();
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
     }
-    return await apiService.getEndpointData(
-        accessToken: _accessToken, endpoint: endpoint);
   }
 }
