61f2b76cdfb2c255182528878bbb54ecfb8bc2af
Eventyret <simen.dehlin@gmail.com>
2020-05-11 20:20:49 +0100

    Updated daashboard with api data

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,6 +1,8 @@
+import 'package:coronavirus_tracker/app/repositories/data_repository.dart';
 import 'package:coronavirus_tracker/app/services/api.dart';
 import 'package:coronavirus_tracker/app/ui/endpoint_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatefulWidget {
   Dashboard({Key key}) : super(key: key);
@@ -10,6 +12,19 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _cases;
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(Endpoint.cases);
+    setState(() => _cases = cases);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +35,7 @@ class _DashboardState extends State<Dashboard> {
         children: <Widget>[
           EndpointCard(
             endpoint: Endpoint.cases,
-            value: 123,
+            value: _cases,
           )
         ],
       ),
