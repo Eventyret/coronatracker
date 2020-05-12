86f3d239be46ba1cc2dff78860bf2327fd0273a9
Eventyret <simen.dehlin@gmail.com>
2020-05-12 14:37:52 +0100

    Finished DataCache service

--- lib/app/services/data_cache_Service.dart
+++ lib/app/services/data_cache_Service.dart
@@ -1,7 +1,9 @@
 import 'package:coronavirus_tracker/app/repositories/endpoints_data.dart';
-import 'api.dart';
+import 'package:coronavirus_tracker/app/services/endpoint_data.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'api.dart';
 
 class DataCacheService {
   DataCacheService({@required this.sharedPreferences});
@@ -9,6 +11,19 @@ class DataCacheService {
 
   static String endpointValueKey(Endpoint endpoint) => '$endpoint/value';
   static String endpointDateKey(Endpoint endpoint) => '$endpoint/date';
+
+  EndpointsData getData() {
+    Map<Endpoint, EndpointData> values = {};
+    Endpoint.values.forEach((endpoint) {
+      final value = sharedPreferences.getInt(endpointValueKey(endpoint));
+      final dateString = sharedPreferences.getString(endpointDateKey(endpoint));
+      if (value != null && dateString != null) {
+        final date = DateTime.tryParse(dateString);
+        values[endpoint] = EndpointData(value: value, date: date);
+      }
+    });
+    return EndpointsData(values: values);
+  }
 
   Future<void> setData(EndpointsData endpointsData) async {
     endpointsData.values.forEach((endpoint, endpointData) async {
