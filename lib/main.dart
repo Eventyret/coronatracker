43a99991b738ef8ec21b8c73e2878408c93d49ca
Eventyret <simen.dehlin@gmail.com>
2020-05-12 14:56:56 +0100

    Made sharedpreferences available during init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:coronavirus_tracker/app/services/data_cache_Service.dart';
 import 'package:coronavirus_tracker/app/ui/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/repositories/data_repository.dart';
 import 'app/services/api.dart';
@@ -10,19 +12,24 @@ import 'app/services/api_service.dart';
 
 void main() async {
   Intl.defaultLocale = 'en_GB';
+  final sharedPrefences = await SharedPreferences.getInstance();
   await initializeDateFormatting();
-  runApp(MyApp());
+  runApp(MyApp(
+    sharedPrefences: sharedPrefences,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key, @required this.sharedPrefences}) : super(key: key);
+  final SharedPreferences sharedPrefences;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Provider<DataRepository>(
       create: (_) => DataRepository(
-        apiService: APIService(
-          API.sandbox(),
-        ),
+        apiService: APIService(API.sandbox()),
+        dataCacheService: DataCacheService(sharedPreferences: sharedPrefences),
       ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
