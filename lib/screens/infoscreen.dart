912f9b44df2035c9f89fb6c5615d6af6e006ebde
Eventyret <simen.dehlin@gmail.com>
2020-05-12 22:41:29 +0100

    Infoscreen now has simple header

---
+++ lib/screens/infoscreen.dart
@@ -0,0 +1,35 @@
+import 'package:coronavirus_tracker/constant.dart';
+import 'package:coronavirus_tracker/widgets/header.dart';
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatelessWidget {
+  const InfoScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          MyHeader(
+            image: "assets/icons/coronadr.svg",
+            textTop: "Get to know",
+            textBottom: "About Covid-19",
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Symptoms",
+                  style: kTitleTextstyle,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
