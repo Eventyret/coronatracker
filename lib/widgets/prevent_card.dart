e3e8874360294be2eaf48221660a954e70fba919
Eventyret <simen.dehlin@gmail.com>
2020-05-12 23:14:31 +0100

    Added padding

--- lib/widgets/prevent_card.dart
+++ lib/widgets/prevent_card.dart
@@ -17,56 +17,59 @@ class PreventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 156,
-      child: Stack(
-        alignment: Alignment.centerLeft,
-        children: <Widget>[
-          Container(
-            height: 136,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 8),
-                    blurRadius: 24,
-                    color: kShadowColor,
-                  ),
-                ]),
-          ),
-          Image.asset(image),
-          Positioned(
-            left: 130,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
               height: 136,
-              width: MediaQuery.of(context).size.width - 170,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    title,
-                    style: kTitleTextstyle.copyWith(
-                      fontSize: 16,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 8),
+                      blurRadius: 24,
+                      color: kShadowColor,
                     ),
-                  ),
-                  Text(
-                    text,
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                  Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset("assets/icons/forward.svg")),
-                ],
-              ),
+                  ]),
             ),
-          )
-        ],
+            Image.asset(image),
+            Positioned(
+              left: 130,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                height: 136,
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: kTitleTextstyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      text,
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset("assets/icons/forward.svg")),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
