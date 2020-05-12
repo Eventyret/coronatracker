0c720546a2e62e90aedfc5637ecf8d9ee68a0677
Eventyret <simen.dehlin@gmail.com>
2020-05-12 22:49:27 +0100

    Extracted to Symtom Card

--- lib/screens/infoscreen.dart
+++ lib/screens/infoscreen.dart
@@ -24,9 +24,55 @@ class InfoScreen extends StatelessWidget {
                 Text(
                   "Symptoms",
                   style: kTitleTextstyle,
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: <Widget>[
+                    SymptomsCard(
+                      image: "assets/images/headache.png",
+                      title: "Headache",
+                    )
+                  ],
                 )
               ],
             ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SymptomsCard extends StatelessWidget {
+  final String image;
+  final String title;
+  const SymptomsCard({
+    Key key,
+    this.image,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 20,
+            color: kActiveShadowColor,
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 90),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
           )
         ],
       ),
