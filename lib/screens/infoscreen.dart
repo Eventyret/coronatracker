bc92d3a51eb2b326a25e386ecf28cbc071ebf74f
Eventyret <simen.dehlin@gmail.com>
2020-05-12 23:10:01 +0100

    Extracted to own file

--- lib/screens/infoscreen.dart
+++ lib/screens/infoscreen.dart
@@ -1,5 +1,6 @@
 import 'package:coronavirus_tracker/constant.dart';
 import 'package:coronavirus_tracker/widgets/header.dart';
+import 'package:coronavirus_tracker/widgets/prevent_card.dart';
 import 'package:coronavirus_tracker/widgets/symptom_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -51,59 +52,11 @@ class InfoScreen extends StatelessWidget {
                 SizedBox(height: 20),
                 Text("Prevention", style: kTitleTextstyle),
                 SizedBox(height: 20),
-                SizedBox(
-                  height: 156,
-                  child: Stack(
-                    alignment: Alignment.centerLeft,
-                    children: <Widget>[
-                      Container(
-                        height: 136,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 8),
-                                blurRadius: 24,
-                                color: kShadowColor,
-                              ),
-                            ]),
-                      ),
-                      Image.asset("assets/images/wear_mask.png"),
-                      Positioned(
-                        left: 130,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
-                          height: 136,
-                          width: MediaQuery.of(context).size.width - 170,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                "Wear face mask",
-                                style: kTitleTextstyle.copyWith(
-                                  fontSize: 16,
-                                ),
-                              ),
-                              Text(
-                                "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks,",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                              Align(
-                                  alignment: Alignment.topRight,
-                                  child: SvgPicture.asset(
-                                      "assets/icons/forward.svg")),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                PreventCard(
+                  text:
+                      "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks,",
+                  image: "assets/images/wear_mask.png",
+                  title: "Wear face mask",
                 ),
               ],
             ),
@@ -113,3 +66,4 @@ class InfoScreen extends StatelessWidget {
     );
   }
 }
+
