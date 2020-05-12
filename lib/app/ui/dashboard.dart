dd2aee1821fad48ce6378413baceea3ab1c4ceba
Eventyret <simen.dehlin@gmail.com>
2020-05-12 08:34:43 +0100

    Added last updated to dashboard

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:coronavirus_tracker/app/repositories/data_repository.dart';
 import 'package:coronavirus_tracker/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_tracker/app/services/api.dart';
 import 'package:coronavirus_tracker/app/ui/endpoint_card.dart';
+import 'package:coronavirus_tracker/app/ui/last_updated_status_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -36,6 +37,9 @@ class _DashboardState extends State<Dashboard> {
         onRefresh: _updateData,
         child: ListView(
           children: <Widget>[
+            LastUpdatedStatusText(text: _endpointsData != null
+                    ? _endpointsData.values[Endpoint.cases].date?.toString() ?? ''
+                    : ''),
             for (var endpoint in Endpoint.values)
               EndpointCard(
                 endpoint: endpoint,
