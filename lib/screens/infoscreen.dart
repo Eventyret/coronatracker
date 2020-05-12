c98109c555fa251e8a725ef1bb48045d9846a067
Eventyret <simen.dehlin@gmail.com>
2020-05-12 23:06:43 +0100

    Alignment of first text

--- lib/screens/infoscreen.dart
+++ lib/screens/infoscreen.dart
@@ -2,6 +2,7 @@ import 'package:coronavirus_tracker/constant.dart';
 import 'package:coronavirus_tracker/widgets/header.dart';
 import 'package:coronavirus_tracker/widgets/symptom_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class InfoScreen extends StatelessWidget {
   const InfoScreen({Key key}) : super(key: key);
@@ -69,7 +70,37 @@ class InfoScreen extends StatelessWidget {
                               ),
                             ]),
                       ),
-                      Image.asset("assets/images/wear_mask.png")
+                      Image.asset("assets/images/wear_mask.png"),
+                      Positioned(
+                        left: 130,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
+                          height: 136,
+                          width: MediaQuery.of(context).size.width - 170,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Wear face mask",
+                                style: kTitleTextstyle.copyWith(
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks,",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Align(
+                                  alignment: Alignment.topRight,
+                                  child: SvgPicture.asset(
+                                      "assets/icons/forward.svg")),
+                            ],
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),
